8c6144c34c5d83906974d58ef3750992ec4ed3fe
Sambhav Jain <sambhav302005@gmail.com>
2024-02-17 10:31:13 +0530

    up

--- peak element/first.py
+++ peak element/first.py
@@ -1,10 +1,11 @@
 l=[1,2,3,4,9,5,6,7,88,9,6,5]
 for i in range(0,len(l)):
     if (i !=0)or(i !=len(l)-1):
+
         if (l[i]>l[i-1])and((l[i]>l[i+1])):
             print(l[i])
         else:
             pass
-    else :
+    elif (i !=0) :
         pass
 
